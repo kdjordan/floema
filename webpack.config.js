00594b396280ab89c3f0e1b363636be3216c1bb1
kdjordan <k.dean.jordan@gmail.com>
2021-05-17 10:26:07 -0700

    webpack complete

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,115 @@ const webpack = require('webpack')
 
 const CopywebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
 const dirApp = path.join(__dirname, 'app')
-const dirAssets = path.join(__dirname, 'assets')
+const dirShared = path.join(__dirname, 'shared')
 const dirStyles = path.join(__dirname, 'styles')
 
-console.log(dirApp, dirAssets, dirStyles)
+const dirNode = 'node_modules'
+
+module.exports = {
+    entry: [
+        path.join(dirApp, 'index.js'),
+        path.join(dirStyles, 'index.scss')
+    ],
+    resolve: {
+        modules: [
+            dirApp,
+            dirShared,
+            dirStyles,
+            dirNode
+        ]
+    },
+    plugins:  [
+        new webpack.DefinePlugin({
+            IS_DEVELOPMENT
+        }),
+        new CopywebpackPlugin({
+            patterns: [
+                {
+                    from: './shared',
+                    to: ''
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+        new ImageMinimizerPlugin({
+            minimizerOptions: {
+              // Lossless optimization with custom option
+              // Feel free to experiment with options for better result for you
+              plugins: [
+                ['gifsicle', { interlaced: true }],
+                ['jpegtran', { progressive: true }],
+                ['optipng', { optimizationLevel: 8 }]
+              ],
+            },
+          }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '',
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ]            
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg|wof2?|fnt|webp)$/,
+                loader: 'file-loader',
+                options: {
+                    name(file) {
+                        return '[hash].[ext]'
+                    }
+                }
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg|webp)$/i,
+                use: [
+                    {
+                        loader: ImageMinimizerPlugin.loader,
+                    },
+                ],
+            },
+            {
+                test: /\.(gsl|frag|vert)$/,
+                loader: 'raw-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(gsl|frag|vert)$/,
+                loader: 'glslify-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+    
+}
